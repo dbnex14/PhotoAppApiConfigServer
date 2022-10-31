659ca0dd685ef39dbd0a055c930077b8cae8165f
dino <mypascal2000@gmail.com>
2022-10-30 17:10:16 -0700

    init as I forgot to create the repo earlier but added properties and also https://github.com/dbnex14/PhotoAppAPIAccountManager.git

---
+++ src/test/java/io/dino/learning/photoappapiconfigserver/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dino.learning.photoappapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
